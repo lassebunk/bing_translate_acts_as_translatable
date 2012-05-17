f6851ade3fe4d63e731f2f159b0171c6696ee4ca
Lasse Bunk <lassebunk@gmail.com>
2012-05-17 13:07:11 +0200

    misc changes

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,5 +1,5 @@
 class Railtie < Rails::Railtie
   rake_tasks do
-    load File.join(File.dirname(__FILE__), "tasks", "translate_task.rake")
+    load File.join(File.dirname(__FILE__), "tasks", "bing_translate_acts_as_translatable_task.rake")
   end
 end
--- lib/tasks/bing_translate_acts_as_translatable_task.rb
+++ lib/tasks/bing_translate_acts_as_translatable_task.rb
@@ -2,38 +2,13 @@ require 'net/http'
 require 'rexml/document'
 
 desc "Translate your YAML files using Bing."
-task :translate => :environment do
+  task :bing_translate_acts_as_translatable => :environment do
+  @class => ENV["class"]
   @from_locale = ENV["from"]
   @to_locale = ENV["to"]
   @app_id = ENV["app_id"] || "TWwfmo09qPdsxVfnXUMCRHAyk2dGOaodKYVOvbyu3m5Y*"
   
   puts "Translating..."
-  
-  source_path = "#{Rails.root}/config/locales/#{@from_locale}.yml"
-  dest_path = "#{Rails.root}/config/locales/#{@to_locale}.yml"
-  
-  if File.exists?(source_path)
-    source_yaml = YAML::load(File.open(source_path))
-    source = source_yaml ? source_yaml[@from_locale] || {} : {}
-  else
-    source = {}
-  end
-
-  if File.exists?(dest_path)
-    dest_yaml = YAML::load(File.open(dest_path))
-    dest = dest_yaml ? dest_yaml[@to_locale] || {} : {}
-  else
-    dest = {}
-  end
-  
-  source = source_yaml ? source_yaml[@from_locale] || {} : {}
-  dest = dest_yaml ? dest_yaml[@to_locale] || {} : {}
-  
-  translated = translate_hash(source)
-  
-  out = { @to_locale => translated.deep_merge(dest) }
-  
-  File.open(dest_path, 'w') {|f| YAML.dump(out, f) }
   
   puts "Done!"
 end
